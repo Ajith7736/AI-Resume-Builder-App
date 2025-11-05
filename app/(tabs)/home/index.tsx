2ec702609e2f6ab096d05142cfadb63c41ce62fb
Ajith7736 <ajith.aju39502@gmail.com>
2025-11-05 23:28:25 +0530

    Update index.tsx

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -3,8 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500 text-base text-2xl ">This is resume builders.</Text>
-      <Text className="text-red-500 text-base text-2xl">This is resume builders.</Text>
+      <Text className="text-red-500 text-2xl ">This is resume builders.</Text>
     </View>
   );
 }
