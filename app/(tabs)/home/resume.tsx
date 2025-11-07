67e7a928264e05f7fdaad3306fdb6836e7d26a39
Ajith7736 <ajith.aju39502@gmail.com>
2025-11-07 23:46:46 +0530

    text

--- app/(tabs)/home/resume.tsx
+++ app/(tabs)/home/resume.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const resume = () => {
     return (
         <SafeAreaView className='flex-1 bg-light-white dark:bg-dark-black'>
-            <Text className='m-5 text-lg text-light-black dark:text-dark-white'>Select Your Resume Template</Text>
+            <Text className='m-5 text-lg text-light-black dark:text-dark-white font-bold text-center'>Select Your Resume Template</Text>
         </SafeAreaView>
     )
 }
